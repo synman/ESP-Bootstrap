2ed300b6bcf60785fa591767fd1e89b6a911aac2
Shell M. Shrader <shell@shellware.com>
2023-10-31 00:59:48 -0400

    preparing release

--- src/Bootstrap.cpp
+++ src/Bootstrap.cpp
@@ -1,4 +1,10 @@
-
+/***************************************************************************
+Copyright © 2023 Shell M. Shrader <shell at shellware dot com>
+----------------------------------------------------------------------------
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+****************************************************************************/
 #include "Bootstrap.h"
 
 AsyncWebServer server(80);
